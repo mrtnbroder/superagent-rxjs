3536d3ccc6f8702aa3615e7a0c74f49571972f41
mrtnbroder <mrtnbroder@gmail.com>
2016-09-29 16:50:23 +0200

    refactor(index): use es6 module syntax

--- src/index.js
+++ src/index.js
@@ -1,29 +1,31 @@
 
-const O = require('rxjs/Observable').Observable
+import { Observable as O } from 'rxjs/Observable'
 
-module.exports = (superagent) => {
-  superagent.Request.prototype.observify = function() {
-    return O.create((o) => {
-      this.end((err, res) => {
-        if (err) {
-          o.error(err)
-        } else {
-          o.next(res)
-        }
-        o.complete()
-      })
-
-      return () => {
-        /*
-         * The response is undefined when the subscriber called unsubscribe
-         * before the request has ended, so let's cancel it.
-         */
-        if (this.response === undefined) {
-          this.abort()
-        }
+const observify = function() {
+  return O.create((o) => {
+    this.end((err, res) => {
+      if (err) {
+        o.error(err)
+      } else {
+        o.next(res)
       }
+      o.complete()
     })
-  }
+
+    return () => {
+      /*
+       * The response is undefined when the subscriber called unsubscribe
+       * before the request has ended, so let's cancel it.
+       */
+      if (this.response === undefined) {
+        this.abort()
+      }
+    }
+  })
+}
+
+export default (superagent) => {
+  superagent.Request.prototype.observify = observify
 
   return superagent
 }
